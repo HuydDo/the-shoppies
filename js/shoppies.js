41a763535247a3db7318d9241dd4fc7065869cca
HuydDo <huydinhdo@yahoo.com>
2020-12-21 09:57:51 -0700

    modified shoppies.js

---
+++ js/shoppies.js
@@ -0,0 +1,7 @@
+const title = document.querySelector('#title')
+const result = document.querySelector('.result')
+const nominateList = document.querySelector('.nominateList')
+
+let count = 0
+let button = document.querySelector('button')
+let nominateArr = []
